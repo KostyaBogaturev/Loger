2cedd611693c46f6ce2cc2480978b88653b7d1ab
Kostya-Bog <59747320+Kostya-Bog@users.noreply.github.com>
2020-11-26 14:24:46 +0200

    Created new project

---
+++ HomeTaskLoger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeTaskLoger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
